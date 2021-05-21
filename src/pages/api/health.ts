914d7182e6f1375056d452ed3d05c3c0667ca73a
Aaron Ward <aaron.l.ward@gmail.com>
2021-05-21 10:37:11 -0400

    rough-in loading of keyVault settings

--- src/pages/api/health.ts
+++ src/pages/api/health.ts
@@ -1,4 +1,7 @@
+import { DefaultAzureCredential } from '@azure/identity';
+import { SecretClient } from '@azure/keyvault-secrets';
 import { NextApiRequest, NextApiResponse } from 'next';
+import getConfig from 'next/config';
 
 import packageJson from '../../../package.json';
 
@@ -7,7 +10,26 @@ interface Health {
   status: string;
 }
 
+async function fetchKeyVaultSecrets() {
+  const { serverRuntimeConfig } = getConfig();
+
+  const keyVaultName = serverRuntimeConfig.azureKeyVaultName;
+  const KVUri = `https://${keyVaultName}.vault.azure.net`;
+
+  const credential = new DefaultAzureCredential();
+  const client = new SecretClient(KVUri, credential);
+
+  const secretName = 'ApplicationInsights--InstrumentationKey';
+  // const appInsightsKey = await client.getSecret(secretName);
+  // const list = await client.listPropertiesOfSecrets();
+
+  for await (const secretProperties of client.listPropertiesOfSecrets()) {
+    console.log('Secret properties: ', secretProperties);
+  }
+}
+
 export default async (req: NextApiRequest, res: NextApiResponse<Health>) => {
+  await fetchKeyVaultSecrets();
   const healthData = { name: packageJson.name, version: packageJson.version, status: 'ok' };
 
   res.status(200).json(healthData);
