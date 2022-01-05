bf3d6067330c24f7960e448b396ea14f57bcf0a0
freakinward <aaron.l.ward@gmail.com>
2022-01-05 11:28:37 -0500

    kudu - install --production --ignore-scripts

--- deploy.sh
+++ deploy.sh
@@ -113,8 +113,7 @@ selectNodeVersion
 if [ -e "$DEPLOYMENT_TARGET/package.json" ]; then
   cd "$DEPLOYMENT_TARGET"
   echo "Running $NPM_CMD install --production"
-#  eval $NPM_CMD install --production
-  eval $NPM_CMD install next
+  eval $NPM_CMD install --production --ignore-scripts
   exitWithMessageOnError "npm failed"
   cd - > /dev/null
 fi
